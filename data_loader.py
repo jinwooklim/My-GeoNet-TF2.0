6ca1030585f392a45cd1f7b833c262f135f2fbe5
Jinwook Lim <aidenlim94@gmail.com>
2019-05-08 14:31:54 +0900

    rev

--- data_loader.py
+++ data_loader.py
@@ -97,6 +97,7 @@ class DataLoader(object):
             src_image_stack = image_all[:, :, :, 3:]
             intrinsics = self.get_multi_scale_intrinsics(intrinsics, opt['num_scales'])
 
+            # Casting is Important
             src_image_stack = tf.cast(src_image_stack, dtype=tf.float32)
             tgt_image = tf.cast(tgt_image, dtype=tf.float32)
             intrinsics = tf.cast(intrinsics, dtype=tf.float32)
